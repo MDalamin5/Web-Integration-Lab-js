202e697824e4d489c7d46d9916e3ea03c0117a3c
mdalamin5 <mdal.amin5@northsouth.edu>
2026-02-22 09:36:49 +0600

    brashup js variable

---
+++ 00-JS-Basics/01-Variable/variable.js
@@ -0,0 +1,11 @@
+const accountId = 18811904
+let accountEmail = "hello@byv.com"
+var accountPassword = "testPass"
+accountCity = "DHK"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+If you want to declear a variable as its never change its will be the const
+and use let if its a dynamic variable. but in var has scoping problem so we did not use it.
+*/
